70b3e1761b151dd336ad3cff23e2078d88518757
gokhan <gokhanerten31@icloud.com>
2022-12-01 02:30:43 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import LoginForm from './component/LoginForm';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
 
 function App() {
+
+
+  const adminUser = {
+  email : "admin@admin.com",
+  password : "admin123"    
+  }
+
+  const [user,setUser] = useState({name: "" , email:""});
+  const [error,setError] = useState("")
+
+
+  const Login = details => {
+    console.log(details)
+
+    if(details.email === adminUser.email && details.password === adminUser.password) {
+      console.log("match")
+      setUser({
+        name:details.name,
+        email:details.email
+      })
+    } else { 
+
+      setError("böyle bir kullanıcı bulunamamaktadır")
+      console.log( "dont match")
+    }
+  }
+
+  const Logout = () => {
+     
+    setUser({
+      name: "" , 
+      
+      email:""
+
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+     {(user.email != "") ? (
+        
+     <div className='welcome'> 
+     
+       <h2>Welcome</h2><span>{user.name}</span>
+      <button onClick={Logout} class="btn btn-primary btn-block mb-4">Logout</button>
+
+     </div>
+     ) : (
+
+      <LoginForm   Login = {Login} error = {error}/>
+     ) }
+
     </div>
   );
-}
+      }
 
 export default App;
